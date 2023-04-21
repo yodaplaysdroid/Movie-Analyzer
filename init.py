6b07fbd04e5deac123f46be4c530fb6718ef4e0e
groot <oscarwongkinnlap@gmail.com>
2023-04-21 21:16:40 +0800

    completed

--- init.py
+++ init.py
@@ -1,9 +1,27 @@
 import pandas as pd
 import pickle
+import os
+import shutil
+import time
 
 
-if __name__ == '__main__':
+# initialization
+def initialize():
+
+    os.system('cls')
+    print('Repairing files ...\n\n')
+    start_time = time.time()
+
+    try:
+        os.mkdir('genome_scores')
+        print('Created : genome_scores')
+    except Exception as e:
+        # print(e)
+        print('Error : genome_scores Exists')
+
     chunk_size = 10 ** 6
+    print('Reading files...')
+    
     df = pd.read_csv('raw/genome-scores.csv', chunksize=chunk_size)
 
     for i, chunk in enumerate(df):
@@ -12,3 +30,64 @@ if __name__ == '__main__':
         for name, group in grouped:
             filename = f'genome_scores/{name}_{i}.csv'
             group.to_csv(filename, index=False)
+            print(f'Created : {filename}')
+    
+    try:
+        os.mkdir('ratings')
+        print('Created : ratings')
+    except Exception as e:
+        # print(e)
+        print('Error : ratings Exists')
+    
+    print('Reading files...')
+
+    df = pd.read_csv('raw/ratings.csv')
+    grouped = df.groupby('userId')
+    
+    for name, group in grouped:
+        filename = f'ratings/{name}.csv'
+        group.to_csv(filename, index=False)
+        print(f'Created : {filename}')
+    
+    try:
+        os.mkdir('scripts/cache')
+        print('Created : scripts/cache')
+    except Exception as e:
+        # print(e)
+        print('Error : scripts/cache Exists')
+    
+    print('Reading Movies Database...')
+    ratings = pd.read_csv('raw/ratings.csv')
+    rating_count = ratings.groupby('movieId')['rating'].count().reset_index(name='rating_count')
+    rating_avg = ratings.groupby('movieId')['rating'].mean().reset_index(name='rating_avg')
+    movies = pd.read_csv('raw/movies.csv')
+    score = rating_count.merge(rating_avg, on='movieId')
+    score = score.merge(movies, on='movieId')
+    r = 3
+    w = 1000
+    score['total'] = (w*r + score['rating_count']*score['rating_avg']) / (w + score['rating_count'])
+    score = score.sort_values('total', ascending=False)
+    score.to_csv('score.csv', index=False)
+    
+    end_time = time.time()
+    time_taken = end_time - start_time
+    print('\n\nOperation finished')
+    print(f'Time Taken : {time_taken}s')
+    input('Press any key to continue >> ')
+
+
+
+# cache clearing
+def clear_cache():
+
+    try:
+        shutil.rmtree('scripts/cache')
+        print('Cache Cleared')
+    except Exception as e:
+        print('No Cache')
+    
+    os.mkdir('scripts/cache')
+
+
+if __name__ == '__main__':
+    pass
---
+++ main.py
@@ -0,0 +1,119 @@
+import Recommender
+import Init
+import User
+import os
+import time
+
+
+def display_menu():
+
+    os.system('cls')
+    print('Movie Recommender System\n')
+    print('1 - Search')
+    print('2 - Login')
+    print('3 - Repair Files')
+    print('4 - Clear Cache')
+    print('x - Exit\n')
+    return input('>> ')
+
+
+def search_movie():
+
+    os.system('cls')
+    print('Search for Movies\n')
+    phrase = input('Search : ')
+
+    start_time = time.time()
+    movies = Recommender.search(phrase)
+    end_time = time.time()
+
+    print('\nResults\n')
+
+    for movie in movies[0:10]:
+        print(movie)
+    
+    time_taken = end_time - start_time
+
+    print('')
+    print(f'Time taken : {time_taken}s')
+    user_input = input('Press any key to continue (x to exit) >> ')
+    
+    if user_input == 'x':
+        return 1
+    else:
+        return 0
+
+
+def user_login():
+
+    os.system('cls')
+    print('User Login\n')
+    user_id = input('User ID : ')
+
+    try:
+        user_id = int(user_id)
+
+        if (user_id >= 0) and (user_id <= 162541):
+            user = User.User(user_id)
+
+            start_time = time.time()
+            print('\nResults\n')
+            for movie in user.recommend():
+                print(movie)
+            end_time = time.time()
+            time_taken = end_time - start_time
+            print('')
+            print(f'Time taken : {time_taken}s')
+            user_input = input('Press any key to continue (x to exit) >> ')
+        
+        else:
+            print('\nUser does not exist')
+            user_input = input('Press any key to continue (x to exit) >> ')
+
+    except Exception as e:
+        print('')
+        print(e)
+        user_input = input('Press any key to continue (x to exit) >> ')
+    
+    if user_input == 'x':
+        return 1
+    else:
+        return 0
+    
+
+if __name__ == '__main__':
+
+    while True:
+        user_input = display_menu()
+
+        if user_input == 'x':
+            break
+
+        elif user_input == '1':
+            if search_movie() == 1:
+                break
+
+        elif user_input == '2':
+            if user_login() == 1:
+                break
+
+        elif user_input == '3':
+            print('\nFile Repairing might take quite some time')
+            user_input = input('Type "yes" to continue: ')
+            print('')
+
+            if user_input == 'yes':
+                Init.initialize()
+            else:
+                print('Operation aborted...')
+                input('>> ')
+
+        elif user_input == '4':
+            print('')
+            Init.clear_cache()
+            input('Press any key to continue >> ')
+
+        else:
+            pass
+    
+    print('\nThanks you and have a GREAT day ... \n')
